c4d3e364d347bff50eb9d1a7286ef9a31ce784ec
habibb2r <habibhk127@gmail.com>
2024-11-18 21:40:37 +0600

    Completed Top Rated Page

--- src/Layouts/Pages/PageTopRated/PageTopRated.jsx
+++ src/Layouts/Pages/PageTopRated/PageTopRated.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import useTopRated from "../../../Hooks/useTopRated";
 import Title from "../../../ReUsable/Title";
+import MovieCard from "../../../ReUsable/MovieCard";
 
 
 const PageTopRated = () => {
@@ -9,7 +10,9 @@ const PageTopRated = () => {
     return (
         <div>
             <Title title='Top Rated Movies'></Title>
-
+            <div className="py-5 grid grid-cols-1 md:grid-cols-3 gap-10 px-[3%]">
+                {topRated?.results.map(movie => <MovieCard key={movie.id} item={movie}></MovieCard>)}
+            </div>
             
         </div>
     );
