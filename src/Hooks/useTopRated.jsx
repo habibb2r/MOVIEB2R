625201ac764b5f8eb3427087ca4e93998267fd5e
habibb2r <habibhk127@gmail.com>
2024-11-18 21:33:06 +0600

    Set up pages

--- src/Hooks/useTopRated.jsx
+++ src/Hooks/useTopRated.jsx
@@ -1,8 +1,9 @@
 import { useQuery } from "@tanstack/react-query";
 
 
-const useTopRated = () => {
+const useTopRated = (page) => {
   const key = import.meta.env.VITE_AUTHKEYMOVIE
+  let pageNo = page || 1
     const {data : topRated= [],  isLoading: loading, refetch } = useQuery({
         queryKey: ['topRated'],
         queryFn: async()=>{
@@ -13,7 +14,7 @@ const useTopRated = () => {
                   Authorization: `${key}`
                 }
               };
-              const res = await fetch(`${import.meta.env.VITE_MOVIELINK}/3/movie/top_rated?page=1`, options);
+              const res = await fetch(`${import.meta.env.VITE_MOVIELINK}/3/movie/top_rated?page=${pageNo}`, options);
               return res.json();
         }
     })
--- src/Layouts/Pages/PageTopRated/PageTopRated.jsx
+++ src/Layouts/Pages/PageTopRated/PageTopRated.jsx
@@ -1,10 +1,15 @@
+import { useState } from "react";
+import useTopRated from "../../../Hooks/useTopRated";
 import Title from "../../../ReUsable/Title";
 
 
 const PageTopRated = () => {
+    const [page, setPage] = useState(1)
+    const [topRated, loading, refetch] = useTopRated(page)
     return (
         <div>
             <Title title='Top Rated Movies'></Title>
+
             
         </div>
     );
